debe95b6cefdc95ec65f94d2f81172a2c27b84a2
רחלי בינט <rachelbinet@MacBook-Air-sl-rhly.local>
2025-11-06 17:37:12 +0200

    first try

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,25 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
-    }
+        String time = args[0];
+        int minutesToAdd = Integer.parseInt(args[1]);
+        //i found the index of the ":" and then did substring to extract the hours
+        int hours = Integer.parseInt(time.substring(time.indexOf(":") - 2, time.indexOf(":")));
+        int minutes = Integer.parseInt(time.substring(time.indexOf(":") +1, time.indexOf(":")+3));
+        int Totalminutes = ((hours * 60) + minutes + minutesToAdd); 
+        int Totalhours = Totalminutes/60;
+        int NewHours = Totalhours % 24;
+        int Newminutes = Totalminutes - (Totalhours * 60);
+        String strHours = String.valueOf(NewHours);
+        String strMinutes = String.valueOf(Newminutes);
+         if (NewHours < 10) {
+            strHours = "0" + strHours;
+        }
+
+        if (Newminutes < 10) {
+            strMinutes = "0" + strMinutes;
+        }
+
+
+        System.out.println(strHours+":"+strMinutes);
+            }
 }
